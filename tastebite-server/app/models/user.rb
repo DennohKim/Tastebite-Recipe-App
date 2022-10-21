d301358afb682f0f82b146efa58cce750529839a
John Kamau <johnjkkamau@gmail.com>
2022-10-21 11:27:47 +0300

    feat:created user model & migration

---
+++ tastebite-server/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+    validates :password, presence: true, length: { in: 6..20 }
+    validates :image_url, presence: true
+end
