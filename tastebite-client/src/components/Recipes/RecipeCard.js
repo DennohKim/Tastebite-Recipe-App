9cbf60d7b37f791c71a9d184d82703c44c166dc3
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-22 11:32:08 +0300

    style: add lg screen media query

--- tastebite-client/src/components/Recipes/RecipeCard.js
+++ tastebite-client/src/components/Recipes/RecipeCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Cooking } from "../../assets/images";
 import { BiTimeFive, BiDish } from "react-icons/bi";
 
 const RecipeCard = ({
@@ -15,41 +14,37 @@ const RecipeCard = ({
     <div>
       <Link to="/signin">
         <div className="shadow-md p-4 rounded-md">
-          <div>
-            <img src={image} alt="recipe" />
+          <div className="">
+            <img className="max-w-full h-auto" src={image} alt="recipe" />
           </div>
-          <p className="pt-4 text-lg md:text-xl lg:text-2xl font-bold ">
+          <p className="pt-4 text-base md:text-base lg:text-md font-bold ">
             {recipeName}
           </p>
           <div className="flex justify-between py-4 ">
-            <div className="flex justify-center items-center">
-              <BiTimeFive/>
-              <p className="pl-4" >{cookingTime}</p>
-              <p className="pl-2">Minutes</p>
-              
+            <div className="flex justify-center items-center ">
+              <BiTimeFive className="" />
+              <p className="pl-2 text-sm md:text-sm lg:text-base">{cookingTime}</p>
+              <p className="pl-2 text-sm md:text-sm lg:text-base">Minutes</p>
             </div>
             <div className="flex justify-center items-center">
-              <BiDish/>
-              <p className="pl-4">{peopleServed}</p>
-              <p className="pl-2">Servings</p>
-              
+              <BiDish />
+              <p className="pl-2 text-sm md:text-sm lg:text-base">{peopleServed}</p>
+              <p className="pl-2 text-sm md:text-sm lg:text-base">Servings</p>
             </div>
           </div>
           <div className="flex justify-between items-center py-2 ">
             <div>
-              
-              <p className="w-full active:scale-90 bg-secondary-color transition duration-150 ease-in-out rounded-full text-white px-8 py-3 text-sm">{country}</p>
+              <p className="w-full active:scale-90 bg-secondary-color transition duration-150 ease-in-out rounded-full text-white px-4 py-2 text-sm">
+                {country}
+              </p>
             </div>
-            <div className="flex gap-2">
-            {Array(ratings).fill().map((_, i) => (
-              <p>⭐</p>
-
-            ))}
-              
+            <div className="flex gap-2 scale-75">
+              {Array(ratings)
+                .fill()
+                .map((_, i) => (
+                  <p>⭐</p>
+                ))}
             </div>
-          </div>
-          <div>
-           
           </div>
         </div>
       </Link>
--- tastebite-client/tailwind.config.js
+++ tastebite-client/tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
-    
-
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'primary-color': '#FFEA74',
-        'secondary-color': '#FF785B',
-        
+        "primary-color": "#FFEA74",
+        "secondary-color": "#FF785B",
+      },
+      width: {
+        215: "215px",
+        357: "357px",
+        557: "557px",
+      },
+      minWidth: {
+        155: "155px",
+        190: "190px",
+        215: "215px",
+        240: "240px",
+        256: "256px",
+        327: "327px",
+      },
+      height: {
+        300: "300px",
+        557: "557px",
       },
     },
+    
   },
-
-}
+};
