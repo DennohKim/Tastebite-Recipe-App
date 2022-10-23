daa6ae80c452167581414f20cec1bbd97b78126c
John Kamau <johnjkkamau@gmail.com>
2022-10-23 12:36:16 +0300

    feat:Created users controller & routes

---
+++ tastebite-server/app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    #Authorization will only skip signup action
+    skip_before_action :authorize, only: :create
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create 
+      user = User.create!(user_params) 
+      session[:user_id] = user.id 
+      render json: user, status: :created 
+    end
+
+    #Return a user who has their id stored in session
+    def show 
+      render json: @current_user
+    end
+    
+    #Return an updated user 
+    def update
+        render json: @current_user.update!(user_params)
+    end
+  
+    private 
+  
+    def user_params 
+      params.permit(:username, :email, :password, :password_confirmation, :image_url) 
+    end
+  
+end
--- tastebite-server/config/routes.rb
+++ tastebite-server/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  get "/users", to: "users#index"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
