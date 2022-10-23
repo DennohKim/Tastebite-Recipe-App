12d6ef3bcf44099c13aaeca1420e470658c1e377
cyron arieri <ariericyron@gmail.com>
2022-10-23 22:57:35 +0300

    doc:added a paragraph

--- tastebite-client/src/components/Banner.js
+++ tastebite-client/src/components/Banner.js
@@ -4,7 +4,10 @@ export default function Banner() {
   return (
     <div className='bg-yellow-300 h-96 pt-8 mt-10'>
         <hi class = ''>Cook, Save and Share </hi>
-    
+        <p class = ''>Taste Bite is a unique recipe sharing and discovery platform, 
+            <br></br>where users can Save their favorite recipes in the app, create 
+            <br></br>new ones and share them with friends.
+        </p>
    
     </div>
   )
