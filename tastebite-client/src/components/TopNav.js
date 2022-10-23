8fe681a714aa166ae9be11be295bae16fa8a5cdd
cyron arieri <ariericyron@gmail.com>
2022-10-23 15:00:46 +0300

    style:styled my flex container

--- tastebite-client/src/components/TopNav.js
+++ tastebite-client/src/components/TopNav.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+
 export default function TopNav() {
   return (
-    <div>TopNav</div>
+    /*flex container*/
+    <div className='flex items-centre justify-between'>
+        TopNav
+    </div>
   )
 }
