d2c2bfd37bce1fcc651c3b3a3880326b3fad1ebe
cyron arieri <ariericyron@gmail.com>
2022-10-23 22:55:44 +0300

    doc:added title

--- tastebite-client/src/components/Banner.js
+++ tastebite-client/src/components/Banner.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Banner() {
   return (
     <div className='bg-yellow-300 h-96 pt-8 mt-10'>
-
+        <hi class = ''>Cook, Save and Share </hi>
+    
+   
     </div>
   )
 }
