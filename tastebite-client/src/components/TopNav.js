ba32d0d55470c297ac8450bbb3e2263bb8f3c999
cyron arieri <ariericyron@gmail.com>
2022-10-24 17:48:36 +0300

    fix:made my website responsive

--- tastebite-client/src/components/TopNav.js
+++ tastebite-client/src/components/TopNav.js
@@ -8,7 +8,7 @@ export default function TopNav() {
         <div className='pt-2 pl-'>
             <img src={logo} alt="logo"></img>
         </div>
-        <div className='flex-6 '>
+        <div className='flex-6 hidden md:flex'>
             <ul className='flex justify-center m-0 p-12 list-none text-lg font-light cursor-pointer mr-9'>
                 <li className='mr-10 hover:text-yellow-300'>Home</li>
                 <li className='mr-10 hover:text-yellow-300'>Recipes</li>
--- tastebite-client/tailwind.config.js
+++ tastebite-client/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
