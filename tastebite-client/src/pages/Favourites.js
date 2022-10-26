43c37385d1a2ac068d3b41f1e84c0d70bb74d753
OnanThairu <thairuonan@gmail.com>
2022-10-26 11:23:22 +0300

    chore: Style recipe list.

--- tastebite-client/src/pages/Favourites.js
+++ tastebite-client/src/pages/Favourites.js
@@ -3,6 +3,8 @@ import Sidebar from "../components/Sidebar";
 import { useStateContext } from "../context/ContextProvider";
 import NavbarDashboard from "../components/NavbarDashboard";
 // import { AiOutlineSearch } from "react-icons/ai"
+import RecipesList from "../components/Recipes/RecipesList";
+import { recipeInfo } from "../data/recipedata";
 
 const Favourites = () => {
   const { activeMenu } = useStateContext();
@@ -31,36 +33,39 @@ const Favourites = () => {
         >
           <div className="fixed md:static bg-main-bg navbar w-full">
             <NavbarDashboard />
-            <div className="bg-secondary-color ml-6">
+            <div className=" ml-[10%]">
               <div className="w-1/4">
                 <p className="font-bold text-xl">Favourites</p>
               </div>
               <div>
-                <div className="bg-gray-200 mt-8">
+                <div className="mt-8">
                   <label htmlFor="site-search"></label>
-                  <input type="search" id="site-search" placeholder="🔍 Search" className="w-1/4" />
+                  <input type="search" id="site-search" placeholder="🔍 Search" className="w-1/4 pl-2 py-1 mr-10 border-solid border-2 border-gray-300 rounded" />
 
                   <label htmlFor="ingredient"></label>
-                  <select name="ingredient" id="ingredient">
+                  <select name="ingredient" id="ingredient" className="mr-3 py-1 px-3 border-solid border-2 border-gray-300 rounded text-gray-400">
                     <option>Kachumbari</option>
                     <option>Avocado</option>
                   </select>
 
                   <label htmlFor="category"></label>
-                  <select name="category" id="category">
+                  <select name="category" id="category" className="mr-3 py-1 px-3 border-solid border-2 border-gray-300 rounded text-gray-400">
                     <option>Breakfast</option>
                     <option>Lunch</option>
                     <option>Supper</option>
                   </select>
 
                   <label htmlFor="country"></label>
-                  <select name="country" id="country">
+                  <select name="country" id="country" className="mr-3 py-1 px-3 border-solid border-2 border-gray-300 rounded text-gray-400">
                     <option>Kenya</option>
                     <option>Morocco</option>
                   </select>
                 </div>
-              </div>
-          </div>
+              </div>  
+            </div>
+            <div>
+              <RecipesList recipes={ recipeInfo } className="p-0" />
+            </div>
           </div>
           
         </div>
