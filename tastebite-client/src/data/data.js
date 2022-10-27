30e03b1a0df0f154fef77ccca9d3c1f6c78bcf99
annastacia <annastaciangina96@gmail.com>
2022-10-27 11:09:38 +0300

    edit:added picture

--- tastebite-client/src/data/data.js
+++ tastebite-client/src/data/data.js
@@ -1,7 +1,7 @@
 export const people =[
     {
         id: 1,
-        image:"avatar1.jpg",
+        image:"https://avatars.githubusercontent.com/u/63696529?s=120&v=4",
         name : "Harvy Neson",
         title :"chef",
         quote:"I think we've been using Taste Bite for a while now and it's an awesome app.It's very easy to use and you can save recipes in it. I think the best thing about this app is that it has alot of categories,so you are ablee to find the perfect recipe for any occassion. ",
@@ -10,7 +10,7 @@ export const people =[
 
     {
         id: 2,
-        image:"avatar2.jpg",
+        image:"https://th.bing.com/th/id/R.c571d0e5d938a891fbe1630635ecc38f?rik=7wiUdBkzBOTtpA&pid=ImgRaw&r=0",
         name : "Johnson Mwendwa",
         title :"chef",
         quote:"I have a lot of food allergies and i'm always excited to find new recipes that i can cook. Taste Bite has a large collection of recipes with photos, ingredients and instructions."
@@ -18,7 +18,7 @@ export const people =[
 
     ,{
         id: 3,
-        image:"avatar3.jpg",
+        image:"https://th.bing.com/th/id/R.c571d0e5d938a891fbe1630635ecc38f?rik=7wiUdBkzBOTtpA&pid=ImgRaw&r=0",
         name : "Colson Walker",
         title :"chef",
         quote:"I have alot of food allergies and allel love this app! I can share my favorite recipes with my friends and family. And it's so easy to use , even my grandmother can do it!rgiesand i'm always excited to fid new recipest that i can cook.Taste Bite has a large collection of recipes with photos, ingredients and instructions. "
