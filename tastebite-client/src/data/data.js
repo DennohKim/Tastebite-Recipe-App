c9fb9baf242c1e57bd556a475d0d19a5d17a7156
annastacia <annastaciangina96@gmail.com>
2022-10-27 11:40:55 +0300

    Test:done

--- tastebite-client/src/data/data.js
+++ tastebite-client/src/data/data.js
@@ -3,16 +3,16 @@ export const people =[
         id: 1,
         image:"https://avatars.githubusercontent.com/u/63696529?s=120&v=4",
         name : "Harvy Neson",
-        title :"chef",
+        title :"student",
         quote:"I think we've been using Taste Bite for a while now and it's an awesome app.It's very easy to use and you can save recipes in it. I think the best thing about this app is that it has alot of categories,so you are ablee to find the perfect recipe for any occassion. ",
     }
     ,
 
     {
         id: 2,
-        image:"https://ca.slack-edge.com/T0101L740P4-U03CT6URCNP-d26c20f47ae1-512",
+        image:"https://th.bing.com/th/id/R.718742d70bb5129db9ac83d699cb6aca?rik=sxpvTvzS8%2f2R%2fA&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2fwife%2fwife-01.jpg&ehk=ue6Y4Uwkuu2f%2fYjBUVhNlR37HO9tk9CcKRrVAtTjLak%3d&risl=&pid=ImgRaw&r=0",
         name : "Johnson Mwendwa",
-        title :"chef",
+        title :"wife",
         quote:"I have a lot of food allergies and i'm always excited to find new recipes that i can cook. Taste Bite has a large collection of recipes with photos, ingredients and instructions."
     }
 
