a27129e4416f9a093e07ae5493f135fde165df10
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-29 12:48:16 +0300

    style: add delete modal

--- tastebite-client/src/App.js
+++ tastebite-client/src/App.js
@@ -10,14 +10,18 @@ import {
   Signin,
   Signup,
   Favourites,
+  ShowRecipe,
+  CreateRecipe,
+  ShowMyRecipes,
+  EditRecipes
+
+
 } from "./pages";
 import "./App.css";
 
-import ShowRecipe from "./pages/ShowRecipe";
+
 import ProtectedRoute from "./components/ProtectedRoute";
-import CreateRecipe from "./pages/CreateRecipe";
-import ShowMyRecipes from "./pages/ShowMyRecipes";
-// import EditRecipes from "./pages/EditRecipes";
+
 
 function App() {
   const { user } = useStateContext();
@@ -93,14 +97,14 @@ function App() {
             </ProtectedRoute>
           }
         ></Route>
-        {/* <Route
+        <Route
           path="/myrecipes/:id/edit"
           element={
             <ProtectedRoute user={user}>
               <EditRecipes />
             </ProtectedRoute>
           }
-        ></Route> */}
+        ></Route>
       </Routes>
     </div>
   );
--- tastebite-client/src/components/MyRecipe/MyAllRecipeCard.js
+++ tastebite-client/src/components/MyRecipe/MyAllRecipeCard.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { BiTimeFive, BiDish } from "react-icons/bi";
 import { FiEdit } from "react-icons/fi";
-import { AiOutlineDelete } from "react-icons/ai";
-
+import DeleteModal from "./DeleteModal";
 
 const MyAllRecipeCard = ({
   image,
@@ -12,12 +11,13 @@ const MyAllRecipeCard = ({
   peopleServed,
   country,
   ratings,
-  recipeId
+  recipeId,
 }) => {
+ 
   return (
     <div>
-      <Link to={`/myrecipes/${recipeId}`}>
       <div className="shadow-md rounded-md">
+        <Link to={`/myrecipes/${recipeId}`}>
           <div className="w-full">
             <img
               className="w-full h-48 object-cover"
@@ -25,56 +25,58 @@ const MyAllRecipeCard = ({
               alt="recipe"
             />
           </div>
-          <div className="px-4">
-            <p className="pt-4 text-base md:text-base lg:text-md font-bold ">
-              {recipeName}
-            </p>
-            <div className="flex justify-between py-4 ">
-              <div className="flex justify-center items-center ">
-                <BiTimeFive className="" />
-                <p className="pl-2 text-sm md:text-sm lg:text-base">
-                  {cookingTime}
-                </p>
-                <p className="pl-2 text-sm md:text-sm lg:text-base">Minutes</p>
-              </div>
-              <div className="flex justify-center items-center">
-                <BiDish />
-                <p className="pl-2 text-sm md:text-sm lg:text-base">
-                  {peopleServed}
-                </p>
-                <p className="pl-2 text-sm md:text-sm lg:text-base">Servings</p>
-              </div>
+        </Link>
+        <div className="px-4">
+          <p className="pt-4 text-base md:text-base lg:text-md font-bold ">
+            {recipeName}
+          </p>
+          <div className="flex justify-between py-4 ">
+            <div className="flex justify-center items-center ">
+              <BiTimeFive className="" />
+              <p className="pl-2 text-sm md:text-sm lg:text-base">
+                {cookingTime}
+              </p>
+              <p className="pl-2 text-sm md:text-sm lg:text-base">Minutes</p>
             </div>
-            <div className="flex justify-between items-center py-2 ">
-              <div>
-                <p className="w-full active:scale-90 bg-secondary-color transition duration-150 ease-in-out rounded-full text-white px-4 py-2 text-sm">
-                  {country}
-                </p>
-              </div>
-              <div className="flex gap-2 scale-75">
-                {Array(ratings)
-                  .fill()
-                  .map((_, i) => (
-                    <p>⭐</p>
-                  ))}
-              </div>
-            </div>
-            <div className="border-b pt-4">
-
-            </div>
-            <div className="flex justify-between align-center py-4  ">
-              <div className="flex gap-2 align-items-center  text-secondary-color active:bg-secondary-color uppercase text-md px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
-                <FiEdit className="self-center" />
-                <button className="self-center" >Edit</button>
-              </div>
-              <div className="flex gap-2 align-center bg-red-500 text-white active:bg-red-800 uppercase text-md px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none  ease-linear transition-all duration-150">
-                <AiOutlineDelete className="self-center" />
-                <button className="self-center" >Delete</button>
-              </div>
+            <div className="flex justify-center items-center">
+              <BiDish />
+              <p className="pl-2 text-sm md:text-sm lg:text-base">
+                {peopleServed}
+              </p>
+              <p className="pl-2 text-sm md:text-sm lg:text-base">Servings</p>
             </div>
           </div>
+          <div className="flex justify-between items-center py-2 ">
+            <div>
+              <p className="w-full active:scale-90 bg-secondary-color transition duration-150 ease-in-out rounded-full text-white px-4 py-2 text-sm">
+                {country}
+              </p>
+            </div>
+            <div className="flex gap-2 scale-75">
+              {Array(ratings)
+                .fill()
+                .map((_, i) => (
+                  <p>⭐</p>
+                ))}
+            </div>
+          </div>
+          <div className="border-b pt-4"></div>
+          <div className="flex justify-between align-center py-4  ">
+            <div>
+              <Link
+                to={`/myrecipes/${recipeId}/edit`}
+                className="flex gap-2 align-items-center  text-secondary-color active:bg-secondary-color text-md px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+                
+              >
+                <FiEdit className="self-center" />
+                Edit
+              </Link>
+            </div>
+
+            <DeleteModal recipeName={recipeName} />
+          </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
