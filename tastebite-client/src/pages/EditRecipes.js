2bb7934b323e8789aedbb0611640d1efca514e7c
Dennis Kimathi <kimathidennis38@gmail.com>
2022-11-02 20:32:39 +0300

    fix: edit recipe

--- tastebite-client/src/pages/EditRecipes.js
+++ tastebite-client/src/pages/EditRecipes.js
@@ -168,13 +168,19 @@ const EditRecipes = () => {
                             />
                           </div> <br/>
 
-                          <div className="mt-16">
+                          <div className="mt-16 flex gap-4">
                             <button
                               className="flex gap-2 align-items-center  text-white bg-secondary-color text-md px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
                               type="submit"
-                              onClick={() =>
-                              setIsEditing((isEditing) => !isEditing)
-                            }
+                              // onClick={() => setIsEditing((isEditing) => !isEditing)}
+                            >
+                              Save Changes
+                            </button>
+
+                            <button
+                              className="flex gap-2 align-items-center  text-white bg-secondary-color text-md px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+                              
+                              onClick={() => setIsEditing((isEditing) => !isEditing)}
                             >
                               Save Changes
                             </button>
